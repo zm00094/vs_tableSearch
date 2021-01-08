cef9060ab35ef4819971eff93dd223a8519c23c6
zm00094 <71019813+zm00094@users.noreply.github.com>
2021-01-08 13:11:44 -0500

    added python tool to convert .csv to .js

---
+++ data2js.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  7 15:28:34 2021
+
+@author: joseph e lucas
+"""
+
+def csv2js(data,dataName,outFile):
+	### Load a csv into 'data'.  dataName is the name of the javascript package, and outFile is the location to write the javascript
+	import pandas as pd
+	header="""(function (global, factory) {
+		typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+		typeof define === 'function' && define.amd ? define(['exports'], factory) :
+		(factory((global.<dataName> = global.<dataName> || {})));
+	}(this, (function (exports) { 'use strict';
+	var data=[""".replace('<dataName>',dataName)
+
+	footer="""   ]
+	exports.data=data;
+	Object.defineProperty(exports, '__esModule', { value: true });
+	})));"""
+
+	cols=list(data)
+
+	f = open(outFile, "w")
+	f.write(header)
+
+	ot='{"'+cols[0]+'":"'+data[cols[0]].str.strip().replace('"',"'")+'"'
+	for lbl in cols[1:]:
+		ot+=',"'+lbl+'":"'+data[lbl].str.replace('"',"'")+'"'
+	ot+='}'
+	ot=ot.str.cat(sep=',\n')
+	f.write(ot)
+	f.write(footer)
+	f.close()
+
+          
